74c858bb7729a148d8206a72d4cb7f2659101a88
scarmonit-creator <scarmonit@scarmonit.com>
2025-10-12 16:37:52 -0400

    Fix ESLint config: separate TypeScript and JavaScript parsing

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,9 @@ import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 
 export default [
+  // Configuration for TypeScript files
   {
-    files: ['**/*.js', '**/*.ts'],
+    files: ['**/*.ts'],
     languageOptions: {
       ecmaVersion: 2024,
       sourceType: 'module',
@@ -13,6 +14,39 @@ export default [
         ecmaVersion: 2024,
         sourceType: 'module',
       },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'no-console': 'off',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+  // Configuration for JavaScript files
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      parser: tsparser,
+      // Note: No parserOptions.project for JS files
       globals: {
         console: 'readonly',
         process: 'readonly',
