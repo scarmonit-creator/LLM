4be0619441d9f15aafb2d095d5d31dd6478434ca
scarmonit-creator <scarmonit@scarmonit.com>
2025-10-12 16:23:28 -0400

    Fix TypeScript parser configuration for ESLint
    Add @typescript-eslint/parser and @typescript-eslint/eslint-plugin to support TypeScript interfaces, enums, and type annotations. This resolves the 5 TypeScript parsing errors in the ESLint workflow.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
 export default [
   {
     files: ['**/*.js', '**/*.ts'],
     languageOptions: {
       ecmaVersion: 2024,
       sourceType: 'module',
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2024,
+        sourceType: 'module',
+      },
       globals: {
         console: 'readonly',
         process: 'readonly',
@@ -12,8 +21,12 @@ export default [
         __filename: 'readonly',
       },
     },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
     rules: {
-      'no-unused-vars': [
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
         'warn',
         {
           argsIgnorePattern: '^_',
