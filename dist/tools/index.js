356e6412993a7959009950186ed618de20da850d
scarmonit-creator <scarmonit@scarmonit.com>
2025-10-14 20:06:10 -0400

    Add missing index.js file in dist/tools

---
+++ dist/tools/index.js
@@ -0,0 +1,10 @@
+// Tool registry and exports
+import BrowserHistoryTool from './browser-history.js';
+
+export { BrowserHistoryTool };
+
+export const tools = {
+  BrowserHistoryTool
+};
+
+export default tools;
